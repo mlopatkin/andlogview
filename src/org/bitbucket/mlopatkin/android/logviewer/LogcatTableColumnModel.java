edd44534e73e5ad4568a3bade0e4cbde5fdeca0a
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-08-04 00:53:32 +0900

    Table headers

--- src/org/bitbucket/mlopatkin/android/logviewer/LogcatTableColumnModel.java
+++ src/org/bitbucket/mlopatkin/android/logviewer/LogcatTableColumnModel.java
@@ -18,44 +18,51 @@ public class LogcatTableColumnModel extends DefaultTableColumnModel {
         int modelColumn;
         int minWidth;
         int maxWidth;
+        String columnName;
         TableCellRenderer renderer;
 
-        public ColumnInfo(int modelColumn, int minWidth, TableCellRenderer renderer) {
+        public ColumnInfo(int modelColumn, String columnName, int minWidth,
+                TableCellRenderer renderer) {
             this.modelColumn = modelColumn;
             this.minWidth = minWidth;
             this.renderer = renderer;
+            this.columnName = columnName;
         }
 
-        public ColumnInfo(int modelColumn, int minWidth, int maxWidth, TableCellRenderer renderer) {
+        public ColumnInfo(int modelColumn, String columnName, int minWidth, int maxWidth,
+                TableCellRenderer renderer) {
             this.modelColumn = modelColumn;
             this.minWidth = minWidth;
             this.maxWidth = maxWidth;
             this.renderer = renderer;
+            this.columnName = columnName;
         }
-        
-        public ColumnInfo(int modelColumn, int minWidth) {
+
+        public ColumnInfo(int modelColumn, String columnName, int minWidth) {
             this.modelColumn = modelColumn;
             this.minWidth = minWidth;
+            this.columnName = columnName;
         }
-        
-        public ColumnInfo(int modelColumn, int minWidth, int maxWidth) {
+
+        public ColumnInfo(int modelColumn, String columnName, int minWidth, int maxWidth) {
             this.modelColumn = modelColumn;
             this.minWidth = minWidth;
             this.maxWidth = maxWidth;
+            this.columnName = columnName;
         }
     }
 
     private Map<String, ColumnInfo> columnInfo = new HashMap<String, ColumnInfo>();
 
     private void initColumnInfo() {
-        columnInfo.put("time",
-                new ColumnInfo(LogRecordsTableModel.COLUMN_TIME, 150, 150, timeCellRenderer));
-        columnInfo.put("pid", new ColumnInfo(LogRecordsTableModel.COLUMN_PID, 30, 50));
-        columnInfo.put("tid", new ColumnInfo(LogRecordsTableModel.COLUMN_TID, 30, 50));
-        columnInfo.put("priority", new ColumnInfo(LogRecordsTableModel.COLUMN_PRIORITY, 30, 50,
+        columnInfo.put("time", new ColumnInfo(LogRecordsTableModel.COLUMN_TIME, "Time", 150, 150,
+                timeCellRenderer));
+        columnInfo.put("pid", new ColumnInfo(LogRecordsTableModel.COLUMN_PID, "pid", 30, 50));
+        columnInfo.put("tid", new ColumnInfo(LogRecordsTableModel.COLUMN_TID, "tid", 30, 50));
+        columnInfo.put("priority", new ColumnInfo(LogRecordsTableModel.COLUMN_PRIORITY, "", 30, 50,
                 priorityCellRenderer));
-        columnInfo.put("tag", new ColumnInfo(LogRecordsTableModel.COLUMN_TAG, 120));
-        columnInfo.put("message", new ColumnInfo(LogRecordsTableModel.COLUMN_MSG, 1000,
+        columnInfo.put("tag", new ColumnInfo(LogRecordsTableModel.COLUMN_TAG, "Tag", 120));
+        columnInfo.put("message", new ColumnInfo(LogRecordsTableModel.COLUMN_MSG, "Message", 1000,
                 messageCellRenderer));
     }
 
@@ -69,6 +76,7 @@ public class LogcatTableColumnModel extends DefaultTableColumnModel {
         if (info.maxWidth != 0) {
             column.setMaxWidth(info.maxWidth);
         }
+        column.setHeaderValue(info.columnName);
         addColumn(column);
     }
 
