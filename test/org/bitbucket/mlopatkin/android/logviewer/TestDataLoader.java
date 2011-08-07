d0b37aab2428a0fff829131fe582e0a761965621
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-08-07 16:11:14 +0900

    Updated source tree to separate tests

--- test/org/bitbucket/mlopatkin/android/logviewer/TestDataLoader.java
+++ test/org/bitbucket/mlopatkin/android/logviewer/TestDataLoader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package test;
+package org.bitbucket.mlopatkin.android.logviewer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
