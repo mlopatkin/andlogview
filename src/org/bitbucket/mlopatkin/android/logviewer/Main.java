d23b514862a1857158675764a6e3ba4bbc6d5f39
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-08-08 16:55:09 +0900

    Full-text search

--- src/org/bitbucket/mlopatkin/android/logviewer/Main.java
+++ src/org/bitbucket/mlopatkin/android/logviewer/Main.java
@@ -17,17 +17,25 @@ package org.bitbucket.mlopatkin.android.logviewer;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
 import java.io.File;
 
+import javax.swing.AbstractAction;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
+import org.apache.log4j.Logger;
 import org.bitbucket.mlopatkin.android.liblogcat.AdbDataSource;
 import org.bitbucket.mlopatkin.android.liblogcat.DataSource;
 import org.bitbucket.mlopatkin.android.liblogcat.DumpstateFileDataSource;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
 
     private JFrame frmAndroidLogViewer;
     private DecoratingRendererTable logElements;
@@ -36,9 +44,12 @@ public class Main {
     private LogRecordTableModel recordsModel = new LogRecordTableModel();
     private AutoScrollController scrollController;
     private FilterController filterController;
+    private SearchController searchController;
     private LogRecordPopupMenuHandler popupMenuHandler;
 
     private DataSource source;
+    private JPanel panel;
+    private JTextField instantSearchTextField;
 
     /**
      * Launch the application.
@@ -101,9 +112,74 @@ public class Main {
         scrollController = new AutoScrollController(logElements, recordsModel);
         filterController = new FilterController(logElements, recordsModel);
         popupMenuHandler = new LogRecordPopupMenuHandler(logElements, filterController);
+        searchController = new SearchController(logElements, recordsModel);
+
+        panel = new JPanel();
+        frmAndroidLogViewer.getContentPane().add(panel, BorderLayout.SOUTH);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        instantSearchTextField = new JTextField();
+        panel.add(instantSearchTextField);
+        instantSearchTextField.setColumns(10);
+        instantSearchTextField.setVisible(false);
 
         JPanel filterPanel = new FilterPanel(filterController);
-        frmAndroidLogViewer.getContentPane().add(filterPanel, BorderLayout.SOUTH);
+        panel.add(filterPanel);
+
+        setupSearchButtons();
     }
 
+    private void setupSearchButtons() {
+        frmAndroidLogViewer.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+                KeyStroke.getKeyStroke(KEY_SHOW_SEARCH_FIELD), ACTION_SHOW_SEARCH_FIELD);
+        frmAndroidLogViewer.getRootPane().getActionMap().put(ACTION_SHOW_SEARCH_FIELD,
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        showSearchField();
+                    }
+                });
+
+        frmAndroidLogViewer.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+                KeyStroke.getKeyStroke(KEY_FIND_NEXT), ACTION_FIND_NEXT);
+        frmAndroidLogViewer.getRootPane().getActionMap().put(ACTION_FIND_NEXT,
+                new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        searchController.continueSearch();
+                    }
+                });
+
+        instantSearchTextField.getInputMap().put(KeyStroke.getKeyStroke(KEY_HIDE_AND_START_SEARCH),
+                ACTION_HIDE_SEARCH_FIELD);
+        instantSearchTextField.getActionMap().put(ACTION_HIDE_SEARCH_FIELD, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideSearchField();
+            }
+        });
+
+    }
+
+    private static final String ACTION_SHOW_SEARCH_FIELD = "show_search";
+    private static final String ACTION_HIDE_SEARCH_FIELD = "hide_search";
+    private static final String ACTION_FIND_NEXT = "find_next";
+    private static final String KEY_HIDE_AND_START_SEARCH = "ENTER";
+    private static final char KEY_SHOW_SEARCH_FIELD = '/';
+    private static final String KEY_FIND_NEXT = "F3";
+
+    private void showSearchField() {
+        instantSearchTextField.setVisible(true);
+        instantSearchTextField.requestFocusInWindow();
+        panel.revalidate();
+        panel.repaint();
+
+    }
+
+    private void hideSearchField() {
+        instantSearchTextField.setVisible(false);
+        searchController.startSearch(instantSearchTextField.getText());
+        panel.revalidate();
+        panel.repaint();
+    }
 }
---
+++ src/org/bitbucket/mlopatkin/android/logviewer/SearchController.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2011 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitbucket.mlopatkin.android.logviewer;
+
+import javax.swing.JTable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bitbucket.mlopatkin.android.liblogcat.LogRecord;
+
+public class SearchController {
+
+    private JTable table;
+    private LogRecordTableModel model;
+    private String text;
+    private int curRow;
+
+    public SearchController(JTable table, LogRecordTableModel model) {
+        this.table = table;
+        this.model = model;
+    }
+
+    public void startSearch(String text) {
+        this.text = text;
+        curRow = table.getSelectedRow();
+        performSearch(curRow >= 0);
+    }
+
+    public void continueSearch() {
+        performSearch(false);
+    }
+
+    private void performSearch(boolean scanCurrentRow) {
+        if (StringUtils.isBlank(text)) {
+            return;
+        }
+        int startPos = (scanCurrentRow) ? curRow : (curRow + 1);
+        for (int i = startPos; i < table.getRowCount(); ++i) {
+            LogRecord record = model.getRowData(table.convertRowIndexToModel(i));
+            if (StringUtils.contains(record.getTag(), text)
+                    || StringUtils.contains(record.getMessage(), text)) {
+                setCurrentRow(i);
+                return;
+            }
+        }
+    }
+
+    private void setCurrentRow(int i) {
+        curRow = i;
+        table.scrollRectToVisible(table.getCellRect(curRow, 0, false));
+        table.getSelectionModel().setSelectionInterval(curRow, curRow);
+    }
+
+}
