58cf94eb53c5a5240988dbd8cceb715b3dc08640
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-08-16 11:55:30 +0900

    Starting '0.10' branch: Integration of ddmlib

--- src/org/bitbucket/mlopatkin/android/liblogcat/AdbDataSource.java
+++ src/org/bitbucket/mlopatkin/android/liblogcat/AdbDataSource.java
@@ -15,67 +15,30 @@
  */
 package org.bitbucket.mlopatkin.android.liblogcat;
 
-import java.awt.EventQueue;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.log4j.Logger;
 
-import org.apache.commons.lang3.text.StrMatcher;
-import org.apache.commons.lang3.text.StrTokenizer;
-import org.bitbucket.mlopatkin.android.logviewer.Configuration;
+import com.android.ddmlib.AndroidDebugBridge;
 
 public class AdbDataSource implements DataSource {
-    private static final String ADB_BASE_COMMANDLINE = Configuration.adb.commandline();
 
-    private LogRecordStream input;
+    private static final Logger logger = Logger.getLogger(AdbDataSource.class);
+
     private LogRecordDataSourceListener listener;
-    private Process adbProcess;
+
+    private AndroidDebugBridge adb;
 
     public AdbDataSource() {
+        AndroidDebugBridge.init(false);
+        adb = AndroidDebugBridge.createBridge();
+        if (adb == null) {
+            logger.error("ADB is null");
+        }
     }
 
+    @Override
     public void close() {
-        adbProcess.destroy();
-    }
+        // TODO Auto-generated method stub
 
-    private List<String> makeCommandLine() {
-        List<String> commands = new ArrayList<String>(splitCommandLine(ADB_BASE_COMMANDLINE));
-        for (String buffer : Configuration.adb.buffers()) {
-            commands.add(Configuration.adb.bufferswitch());
-            commands.add(buffer);
-        }
-        return commands;
-    }
-
-    private static List<String> splitCommandLine(String commandLine) {
-        StrTokenizer tokenizer = new StrTokenizer(commandLine, StrMatcher.splitMatcher(),
-                StrMatcher.quoteMatcher());
-        return tokenizer.getTokenList();
-    }
-
-    private void pushRecord(final LogRecord record) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                listener.onNewRecord(record);
-            }
-        });
-    }
-
-    private class AdbPollingThread extends Thread {
-        AdbPollingThread() {
-            super("ADB-Polling");
-            setDaemon(true);
-        }
-
-        @Override
-        public void run() {
-            LogRecord record = input.next();
-            while (record != null) {
-                pushRecord(record);
-                record = input.next();
-            }
-        }
     }
 
     @Override
@@ -87,14 +50,6 @@ public class AdbDataSource implements DataSource {
     @Override
     public void setLogRecordListener(LogRecordDataSourceListener listener) {
         this.listener = listener;
-        ProcessBuilder pb = new ProcessBuilder(makeCommandLine());
-
-        try {
-            adbProcess = pb.start();
-            input = new LogRecordStream(adbProcess.getInputStream());
-            (new AdbPollingThread()).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
+
 }
