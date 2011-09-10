a3ee7ff32dc6f6c6b5f4926564e3870fe48406ef
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-09-11 02:28:09 +0900

    Added record buffering to keep responsiveness of the UI (fixed #4)

--- src/org/bitbucket/mlopatkin/android/logviewer/BufferedListener.java
+++ src/org/bitbucket/mlopatkin/android/logviewer/BufferedListener.java
@@ -21,9 +21,11 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.Timer;
 
+import org.apache.log4j.Logger;
 import org.bitbucket.mlopatkin.android.liblogcat.LogRecord;
 import org.bitbucket.mlopatkin.android.liblogcat.LogRecordDataSourceListener;
 import org.bitbucket.mlopatkin.android.liblogcat.ddmlib.AdbDataSource;
@@ -38,6 +40,7 @@ import org.bitbucket.mlopatkin.android.liblogcat.ddmlib.AdbDataSource;
  * unresponsive. However, new records should appear as fast as possible.
  */
 public class BufferedListener implements LogRecordDataSourceListener {
+    private static final Logger logger = Logger.getLogger(BufferedListener.class);
 
     private LogRecordTableModel model;
     private AutoScrollController scrollController;
@@ -45,33 +48,103 @@ public class BufferedListener implements LogRecordDataSourceListener {
     public BufferedListener(LogRecordTableModel model, AutoScrollController scrollController) {
         this.model = model;
         this.scrollController = scrollController;
-        mergeTimer.setDelay(500);
         mergeTimer.start();
+        watchdogTimer.start();
     }
+
+    private enum Policy {
+        /**
+         * Send a record to the event queue as soon as it arrives.
+         */
+        IMMEDIATE,
+        /**
+         * Store a record in the buffer and send a batch of them in the merger.
+         */
+        BUFFER;
+    }
+
+    private Policy policy = Policy.IMMEDIATE;
+
+    private static final int MERGE_INTERVAL_MS = 500;
+    private static final int WATCHDOG_INTERVAL_MS = 500;
+
+    // maximal number of records that can be set to eventqueue between watchdog
+    // invocations
+    private static final int MAX_RECORDS_SPEED_THRESHOLD = 50;
+
+    // minimal number of records that arrived between merger invokations to keep
+    // buffering
+    private static final int MIN_RECORDS_SPEED_THRESHOLD = 50;
+
+    // the number of records sent into eventqueue between watchdog invocations
+    private volatile AtomicInteger immediateCount = new AtomicInteger(0);
 
     private List<LogRecord> internalBuffer = new ArrayList<LogRecord>();;
     private final Object lock = new Object();
 
     @Override
     public void onNewRecord(final LogRecord record) {
+        switch (policy) {
+        case IMMEDIATE:
+            sendRecordImmediate(record);
+            break;
+        case BUFFER:
+            addRecordToBuffer(record);
+            break;
+        default:
+            throw new IllegalStateException("Invalid policy value: " + policy);
+        }
+    }
+
+    private void addRecordToBuffer(LogRecord record) {
         synchronized (lock) {
             internalBuffer.add(record);
         }
-
     }
 
-    private Timer mergeTimer = new Timer(0, new ActionListener() {
+    private void sendRecordImmediate(final LogRecord record) {
+        int count = immediateCount.addAndGet(1);
+        if (count >= MAX_RECORDS_SPEED_THRESHOLD) {
+            setPolicy(Policy.BUFFER);
+        }
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                addOneRecord(record);
+            }
+        });
+    }
+
+    private Timer mergeTimer = new Timer(MERGE_INTERVAL_MS, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            mergeIntoModel();
+            if (policy == Policy.BUFFER) {
+                mergeIntoModel();
+            }
         }
     });
+
+    private Timer watchdogTimer = new Timer(WATCHDOG_INTERVAL_MS, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            immediateCount.set(0);
+        }
+    });
+
+    private void addOneRecord(LogRecord record) {
+        assert EventQueue.isDispatchThread();
+        scrollController.notifyBeforeInsert();
+        model.addRecord(record);
+    }
 
     private void mergeIntoModel() {
         assert EventQueue.isDispatchThread();
         List<LogRecord> records = internalBuffer;
         synchronized (lock) {
             internalBuffer = new ArrayList<LogRecord>();
+        }
+        if (records.size() < MIN_RECORDS_SPEED_THRESHOLD) {
+            setPolicy(Policy.IMMEDIATE);
         }
         Collections.sort(records);
         scrollController.notifyBeforeInsert();
@@ -89,4 +162,8 @@ public class BufferedListener implements LogRecordDataSourceListener {
         });
     }
 
+    private void setPolicy(Policy policy) {
+        this.policy = policy;
+        logger.debug("Switched to " + policy);
+    }
 }
