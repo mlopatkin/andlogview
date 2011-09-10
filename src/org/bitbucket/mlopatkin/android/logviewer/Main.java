22b443b1c1018f64c513080325cf065b16ba0729
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-09-11 02:54:25 +0900

    Version 0.16

--- src/org/bitbucket/mlopatkin/android/logviewer/Main.java
+++ src/org/bitbucket/mlopatkin/android/logviewer/Main.java
@@ -34,7 +34,7 @@ import com.android.ddmlib.IDevice;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
 
-    public static final String APP_VERSION = "0.15";
+    public static final String APP_VERSION = "0.16";
 
     private DataSource initialSource;
     private MainFrame window;
