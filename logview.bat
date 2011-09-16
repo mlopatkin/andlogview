4ca3fd8c9e34ba5c18e71aac6f81d4f05cc0ab70
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-09-17 03:57:11 +0900

    Path issue when using tool as file association for .log

--- logview.bat
+++ logview.bat
@@ -1,4 +1,4 @@
 @echo off
-if "%ANDROID_HOME%" == "" (set DDMLIB=lib/ddmlib.jar) else (set DDMLIB=%ANDROID_HOME%\tools\lib\ddmlib.jar)
-set CLASSES=%DDMLIB%;lib/logview.jar
+if "%ANDROID_HOME%" == "" (set DDMLIB=%~dp0\lib\ddmlib.jar) else (set DDMLIB=%ANDROID_HOME%\tools\lib\ddmlib.jar)
+set CLASSES=%DDMLIB%;%~dp0\lib\logview.jar
 start javaw -cp %CLASSES% org.bitbucket.mlopatkin.android.logviewer.Main %*
