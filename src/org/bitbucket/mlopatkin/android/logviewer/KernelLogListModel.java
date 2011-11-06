b842a8fde545feb87d07cd8874097a9bedf7fb07
Mikhail Lopatkin <darth.beleg@gmail.com>
2011-11-06 22:15:45 +0900

    Window for kernel log

---
+++ src/org/bitbucket/mlopatkin/android/logviewer/KernelLogListModel.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2011 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitbucket.mlopatkin.android.logviewer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+
+import org.bitbucket.mlopatkin.android.liblogcat.KernelLogRecord;
+
+/**
+ *
+ */
+public class KernelLogListModel extends AbstractListModel implements
+        BatchRecordsReceiver<KernelLogRecord> {
+
+    private final List<KernelLogRecord> records = new ArrayList<KernelLogRecord>();
+
+    @Override
+    public int getSize() {
+        return records.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return records.get(index);
+    }
+
+    @Override
+    public void addRecord(KernelLogRecord record) {
+        records.add(record);
+        int index = records.size() - 1;
+        fireIntervalAdded(this, index, index);
+    }
+
+    @Override
+    public void addRecords(List<KernelLogRecord> records) {
+        int oldSize = this.records.size();
+        if (records.isEmpty()) {
+            return;
+        }
+        this.records.addAll(records);
+
+        fireIntervalAdded(this, oldSize, this.records.size() - 1);
+    }
+
+    @Override
+    public void setRecords(List<KernelLogRecord> records) {
+        this.records.clear();
+        this.records.addAll(records);
+
+        fireContentsChanged(this, 0, this.records.size());
+    }
+
+}
---
+++ src/org/bitbucket/mlopatkin/android/logviewer/KernelLogWindow.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2011 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bitbucket.mlopatkin.android.logviewer;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+public class KernelLogWindow extends JFrame {
+    private JList kernelLogList;
+
+    public KernelLogWindow() {
+        setBounds(100, 100, 450, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JScrollPane scrollPane = new JScrollPane();
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        kernelLogList = new JList();
+        scrollPane.setViewportView(kernelLogList);
+    }
+
+}
