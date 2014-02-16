9cceeee94887b43e5bb490e1ec4ed5e15303dfcb
Mikhail Lopatkin <darth.beleg@gmail.com>
2014-02-16 20:35:29 +0400

    FluentPredicate

---
+++ test/org/bitbucket/mlopatkin/utils/FluentPredicateTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2014 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitbucket.mlopatkin.utils;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FluentPredicateTest {
+
+    @Test
+    public void testFrom() throws Exception {
+        assertAlwaysFalse(FluentPredicate.from(Predicates.<Integer>alwaysFalse()));
+        assertAlwaysTrue(FluentPredicate.from(Predicates.<Integer>alwaysTrue()));
+        assertLikeIsOdd(FluentPredicate.from(isOdd()));
+
+        assertAlwaysTrue(FluentPredicate.<Integer>from(null));
+    }
+
+    @Test
+    public void testFrom_null() throws Exception {
+        assertAlwaysTrue(FluentPredicate.<Integer>from(null));
+    }
+
+    @Test
+    public void testAnd() throws Exception {
+        FluentPredicate<Integer> fluentIsOdd = FluentPredicate.from(isOdd());
+
+        assertAlwaysFalse(fluentIsOdd.and(Predicates.<Integer>alwaysFalse()));
+        assertLikeIsOdd(fluentIsOdd.and(Predicates.<Integer>alwaysTrue()));
+    }
+
+    @Test
+    public void testOr() throws Exception {
+        FluentPredicate<Integer> fluentIsOdd = FluentPredicate.from(isOdd());
+
+        assertLikeIsOdd(fluentIsOdd.or(Predicates.<Integer>alwaysFalse()));
+        assertAlwaysTrue(fluentIsOdd.or(Predicates.<Integer>alwaysTrue()));
+    }
+
+    @Test
+    public void testNot() throws Exception {
+        FluentPredicate<Integer> fluentAlwaysTrue = FluentPredicate
+                .from(Predicates.<Integer>alwaysTrue());
+
+        assertAlwaysFalse(fluentAlwaysTrue.not());
+    }
+
+    private void assertAlwaysFalse(FluentPredicate<Integer> p) {
+        Assert.assertFalse(p.apply(0));
+        Assert.assertFalse(p.apply(1));
+        Assert.assertFalse(p.apply(null));
+    }
+
+    private void assertAlwaysTrue(FluentPredicate<Integer> p) {
+        Assert.assertTrue(p.apply(0));
+        Assert.assertTrue(p.apply(1));
+        Assert.assertTrue(p.apply(null));
+    }
+
+    private void assertLikeIsOdd(FluentPredicate<Integer> p) {
+        Assert.assertFalse(p.apply(0));
+        Assert.assertTrue(p.apply(1));
+        Assert.assertTrue(p.apply(null));
+    }
+
+    private Predicate<Integer> isOdd() {
+        return new Predicate<Integer>() {
+            @Override
+            public boolean apply(@Nullable Integer integer) {
+                return Objects.firstNonNull(integer, 1) % 2 != 0;
+            }
+        };
+    }
+}
---
+++ utils/org/bitbucket/mlopatkin/utils/FluentPredicate.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2014 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitbucket.mlopatkin.utils;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+/**
+ * Fluent implementation of the Predicate.
+ */
+public abstract class FluentPredicate<T> implements Predicate<T> {
+
+    // don't try to use #from there
+    private static final FluentPredicate<Object> ALWAYS_TRUE = new FluentPredicateWrapper<Object>(
+            Predicates.alwaysTrue());
+
+    public static <T> FluentPredicate<T> from(@Nullable Predicate<T> inner) {
+        if (inner instanceof FluentPredicate) {
+            return (FluentPredicate<T>) inner;
+        }
+        if (inner == null) {
+            return ALWAYS_TRUE.narrow();
+        }
+        return new FluentPredicateWrapper<T>(inner);
+    }
+
+    public FluentPredicate<T> and(@Nullable Predicate<? super T> other) {
+        if (other == null) {
+            return this;
+        }
+        return FluentPredicate.from(Predicates.and(getPredicate(), other)).narrow();
+    }
+
+    public FluentPredicate<T> or(@Nullable Predicate<? super T> other) {
+        if (other == null) {
+            return this;
+        }
+        return FluentPredicate.from(Predicates.or(getPredicate(), other)).narrow();
+    }
+
+    public FluentPredicate<T> not() {
+        return FluentPredicate.from(Predicates.not(getPredicate())).narrow();
+    }
+
+    @SuppressWarnings("unchecked")
+    <U extends T> FluentPredicate<U> narrow() {
+        return (FluentPredicate<U>) this;
+    }
+
+    Predicate<T> getPredicate() {
+        return this;
+    }
+
+    private static class FluentPredicateWrapper<T> extends FluentPredicate<T> {
+
+        private final Predicate<T> inner;
+
+        protected FluentPredicateWrapper(@Nonnull Predicate<T> inner) {
+            this.inner = Preconditions.checkNotNull(inner);
+        }
+
+        @Override
+        public boolean apply(@Nullable T t) {
+            return inner.apply(t);
+        }
+
+        @Override
+        Predicate<T> getPredicate() {
+            return inner;
+        }
+    }
+}
