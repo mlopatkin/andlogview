210722a1c4cd474927c611c468c1e11a4125be62
Mikhail Lopatkin <darth.beleg@gmail.com>
2021-01-31 01:38:59 +0300

    Add an AssertJ-alike checker for the LogRecords

---
+++ test/name/mlopatkin/andlogview/liblogcat/AssertLogRecord.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2021 Mikhail Lopatkin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package name.mlopatkin.andlogview.liblogcat;
+
+import com.google.common.primitives.Ints;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.junit.Assert;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * AssertJ-styled verifier for the LogRecord.
+ */
+public class AssertLogRecord {
+    private final Set<Field> checkedFields = EnumSet.noneOf(Field.class);
+    private final LogRecord item;
+
+    private AssertLogRecord(LogRecord item) {
+        this.item = item;
+    }
+
+    /**
+     * Starts the assertion chain and checks that the argument is not null.
+     *
+     * @param record the record to check
+     * @return the assertion chain builder
+     * @throws AssertionError if the {@code record} is {@code null}
+     */
+    public static AssertLogRecord assertThatRecord(@Nullable LogRecord record) {
+        Assert.assertNotNull(record);
+        return new AssertLogRecord(Objects.requireNonNull(record));
+    }
+
+    /**
+     * Checks that the record has a timestamp set and month and day of this timestamp match arguments. Timestamp is
+     * considered verified after this method.
+     *
+     * @param month the expected month of the timestamp
+     * @param day the expected day of the timestamp
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasDate(int month, int day) {
+        checkedFields.add(Field.TIME);
+        // TODO(mlopatkin) handle the case when the timestamp in the LogRecord actually has year.
+        LocalDate actual = getTimeAsLocalDateTime().toLocalDate();
+        LocalDate expected = LocalDate.of(actual.getYear(), month, day);
+
+        Assert.assertEquals(expected, actual);
+        return this;
+    }
+
+    /**
+     * Checks that the record has a timestamp set and hour, minutes, seconds and milliseconds of this timestamp match
+     * arguments. Timestamp is considered verified after this method.
+     *
+     * @param hour the expected hour of the timestamp
+     * @param min the expected minute of the timestamp
+     * @param sec the expected second of the timestamp
+     * @param msec the expected milliseconds of the timestamp
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasTime(int hour, int min, int sec, int msec) {
+        checkedFields.add(Field.TIME);
+        LocalTime expected = LocalTime.of(hour, min, sec, Ints.checkedCast(TimeUnit.MILLISECONDS.toNanos(msec)));
+        Assert.assertEquals(expected, getTimeAsLocalDateTime().toLocalTime());
+        return this;
+    }
+
+    private LocalDateTime getTimeAsLocalDateTime() {
+        Date time = item.getTime();
+        Assert.assertNotNull("Time is null", time);
+        return time.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+
+    /**
+     * Checks that all the unchecked fields of the record that have defaults are equal to these defaults. This should be
+     * the last check in the chain.
+     *
+     * @throws AssertionError if the check fails
+     */
+    public void andAllOtherFieldAreDefaults() {
+        if (!checkedFields.contains(Field.TIME)) {
+            Assert.assertNull("Time is set (non-default)", item.getTime());
+        }
+        if (!checkedFields.contains(Field.PID)) {
+            Assert.assertEquals("PID is set", LogRecord.NO_ID, item.getPid());
+        }
+        if (!checkedFields.contains(Field.TID)) {
+            Assert.assertEquals("PID is set", LogRecord.NO_ID, item.getTid());
+        }
+        if (!checkedFields.contains(Field.BUFFER)) {
+            Assert.assertNull("Buffer is set (non-default)", item.getBuffer());
+        }
+        if (!checkedFields.contains(Field.APP_NAME)) {
+            Assert.assertEquals("App name is set", "", item.getAppName());
+        }
+    }
+
+    /**
+     * Checks that the record's tag is equal to the given tag.
+     *
+     * @param tag the expected tag of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasTag(String tag) {
+        checkedFields.add(Field.TAG);
+        Assert.assertEquals("Tag mismatch", tag, item.getTag());
+        return this;
+    }
+
+
+    /**
+     * Checks that the record's message is equal to the given one.
+     *
+     * @param message the expected message of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasMessage(String message) {
+        checkedFields.add(Field.MESSAGE);
+        Assert.assertEquals("Message mismatch", message, item.getMessage());
+        return this;
+    }
+
+    /**
+     * Checks that the record's pid is equal to the given one.
+     *
+     * @param pid the expected pid of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasPid(int pid) {
+        checkedFields.add(Field.PID);
+        Assert.assertEquals("Pid mismatch", pid, item.getPid());
+        return this;
+    }
+
+    /**
+     * Checks that the record's tid is equal to the given one.
+     *
+     * @param tid the expected tid of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasTid(int tid) {
+        checkedFields.add(Field.TID);
+        Assert.assertEquals("Tid mismatch", tid, item.getTid());
+        return this;
+    }
+
+    /**
+     * Checks that the record's priority is equal to the given one.
+     *
+     * @param priority the expected priority of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasPriority(LogRecord.Priority priority) {
+        checkedFields.add(Field.PRIORITY);
+        Assert.assertEquals("Priority mismatch", priority, item.getPriority());
+        return this;
+    }
+
+    /**
+     * Checks that the record's buffer is equal to the given one.
+     *
+     * @param buffer the expected buffer of the record
+     * @return the assertion chain builder
+     * @throws AssertionError if the check fails
+     */
+    public AssertLogRecord hasBuffer(LogRecord.Buffer buffer) {
+        checkedFields.add(Field.BUFFER);
+        Assert.assertEquals("Buffer mismatch", buffer, item.getBuffer());
+        return this;
+    }
+}
--- test/name/mlopatkin/andlogview/liblogcat/LogRecordParserTest.java
+++ test/name/mlopatkin/andlogview/liblogcat/LogRecordParserTest.java
@@ -15,101 +15,64 @@
  */
 package name.mlopatkin.andlogview.liblogcat;
 
-import static org.junit.Assert.assertEquals;
+import static name.mlopatkin.andlogview.liblogcat.AssertLogRecord.assertThatRecord;
+
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Test;
 
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 
 public class LogRecordParserTest {
     static final String BRIEF_RECORD = "D/MediaScanner(417): postscan return";
     static final String BRIEF_RECORD_PAD = "D/MediaScanner   (  417): postscan return";
     static final String PROCESS_RECORD = "D(  417) postscan return  (MediaScanner)";
     static final String TAG_RECORD = "D/MediaScanner: postscan return";
-    static final String THREAD_RECORD = "D(  417:0x494) postscan return";
     static final String THREADTIME_RECORD = "08-18 13:40:59.546   417  1172 D MediaScanner: postscan return";
     static final String THREADTIME_RECORD_WITH_MCS =
             "08-18 13:40:59.546789   417  1172 D MediaScanner: postscan return";
-    static final String RAW_RECORD = "postscan return";
     static final String TIME_RECORD = "08-18 13:40:59.546 D/MediaScanner(417): postscan return";
     static final String TIME_RECORD_WITH_MCS = "08-18 13:40:59.546789 D/MediaScanner(417): postscan return";
     static final String TIME_RECORD_PAD = "08-18 13:40:59.546 D/MediaScanner   (  417): postscan return";
-    static final String LONG_RECORD = "[ 08-18 13:40:59.546   417:0x494 D/MediaScanner ]\n"
-            + "postscan return";
 
     static final String TAG = "MediaScanner";
     static final String MESSAGE = "postscan return";
     static final int PID = 417;
     static final int TID = 1172;
     static final LogRecord.Priority PRIORITY = LogRecord.Priority.DEBUG;
-    static final Date DATE = getDate(getCurrentYear(), 8, 18, 13, 40, 59, 546);
     static final LogRecord.Buffer BUFFER = LogRecord.Buffer.MAIN;
 
     static final String LOG_BEGINNING_LINE = "--------- beginning of /dev/log/system";
     static final String BLANK_LINE = "";
-
-    private static final int getCurrentYear() {
-        return Calendar.getInstance().get(Calendar.YEAR);
-    }
-
-    private static Date getDate(int year, int month, int day, int hour, int min, int sec, int msec) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month - 1, day, hour, min, sec);
-        calendar.set(Calendar.MILLISECOND, msec);
-        return calendar.getTime();
-    }
-
-    private static boolean isSameDate(Date expected, @Nullable Date actual) {
-        if (actual == null) {
-            return false;
-        }
-        if (expected.equals(actual)) {
-            return true;
-        }
-        Calendar calExpected = Calendar.getInstance();
-        calExpected.setTime(expected);
-        Calendar calActual = Calendar.getInstance();
-        calActual.setTime(actual);
-        calExpected.set(Calendar.YEAR, calActual.get(Calendar.YEAR));
-        expected = calExpected.getTime();
-        return expected.equals(actual);
-    }
 
     @Test
     public void testLogRecordParserThreadTime() {
         LogRecord record =
                 LogRecordParser.parseThreadTime(BUFFER, THREADTIME_RECORD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertEquals(TID, record.getTid());
-        assertTrue(isSameDate(DATE, record.getTime()));
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasTid(TID)
+                .hasDate(8, 18).hasTime(13, 40, 59, 546)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
     public void testLogRecordParserBrief() {
         LogRecord record = LogRecordParser.parseBrief(BUFFER, BRIEF_RECORD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertNull(record.getTime());
-        assertEquals(LogRecord.NO_ID, record.getTid());
-
-        assertEquals(PID, record.getPid());
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
@@ -117,15 +80,13 @@ public class LogRecordParserTest {
         LogRecord record =
                 LogRecordParser.parseBrief(BUFFER, BRIEF_RECORD_PAD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertNull(record.getTime());
-        assertEquals(LogRecord.NO_ID, record.getTid());
-
-        assertEquals(PID, record.getPid());
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
@@ -133,60 +94,53 @@ public class LogRecordParserTest {
         LogRecord record =
                 LogRecordParser.parseProcess(BUFFER, PROCESS_RECORD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(LogRecord.NO_ID, record.getTid());
-        assertNull(record.getTime());
-
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
     public void testLogRecordParserTag() {
         LogRecord record = LogRecordParser.parseTag(BUFFER, TAG_RECORD);
 
-        assertNotNull(record);
-        assertEquals(LogRecord.NO_ID, record.getTid());
-        assertNull(record.getTime());
-        assertEquals(LogRecord.NO_ID, record.getPid());
-
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
     public void testLogRecordParserTime() {
         LogRecord record = LogRecordParser.parseTime(BUFFER, TIME_RECORD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(LogRecord.NO_ID, record.getTid());
-
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertTrue(isSameDate(DATE, record.getTime()));
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasDate(8, 18).hasTime(13, 40, 59, 546)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
     public void testLogRecordParserTimePad() {
         LogRecord record = LogRecordParser.parseTime(BUFFER, TIME_RECORD_PAD, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(LogRecord.NO_ID, record.getTid());
-
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertTrue(isSameDate(DATE, record.getTime()));
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasDate(8, 18).hasTime(13, 40, 59, 546)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
@@ -203,26 +157,29 @@ public class LogRecordParserTest {
     public void testTimeLogWithMicrosecondsTimestamp() {
         LogRecord record = LogRecordParser.parseTime(BUFFER, TIME_RECORD_WITH_MCS, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertTrue(isSameDate(DATE, record.getTime()));
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasDate(8, 18).hasTime(13, 40, 59, 546)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
     }
 
     @Test
     public void testThreadTimeLogWithMicrosecondsTimestamp() {
         LogRecord record = LogRecordParser.parseThreadTime(BUFFER, THREADTIME_RECORD_WITH_MCS, Collections.emptyMap());
 
-        assertNotNull(record);
-        assertEquals(TAG, record.getTag());
-        assertEquals(MESSAGE, record.getMessage());
-        assertEquals(PID, record.getPid());
-        assertEquals(TID, record.getTid());
-        assertTrue(isSameDate(DATE, record.getTime()));
-        assertEquals(PRIORITY, record.getPriority());
-        assertEquals(BUFFER, record.getBuffer());
+        assertThatRecord(record)
+                .hasTag(TAG)
+                .hasMessage(MESSAGE)
+                .hasPid(PID)
+                .hasTid(TID)
+                .hasDate(8, 18).hasTime(13, 40, 59, 546)
+                .hasPriority(PRIORITY)
+                .hasBuffer(BUFFER)
+                .andAllOtherFieldAreDefaults();
+
     }
 }
