19a4546983233cfecef844d908e80d7a3a87f58d
Mikhail Lopatkin <darth.beleg@gmail.com>
2021-10-18 00:56:53 +0300

    Use custom Python binary name for lint-all.py
    This particular image still uses 'python3' binary name. Docker images
    are a mess.

--- tools/hooks/lint-all.sh
+++ tools/hooks/lint-all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 git ls-files -z | xargs -0 \
-  python tools/hooks/lint.py --ignored-files-list .nolint --files || exit 1
+  ${PYTHON_BINARY:-python} tools/hooks/lint.py \
+  --ignored-files-list .nolint --files || exit 1
