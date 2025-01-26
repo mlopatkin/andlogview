7b7a5279c377560eaa2fe74ce0de716b0ca95714
Mikhail Lopatkin <me@mlopatkin.name>
2025-01-26 18:25:10 +0100

    Fix formatting
    Issue: n/a

--- buildSrc/src/main/kotlin/name/mlopatkin/andlogview/building/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/name/mlopatkin/andlogview/building/java-conventions.gradle.kts
@@ -117,10 +117,15 @@ tasks.withType<JavaCompile>().configureEach {
         option("NullAway:AnnotatedPackages", "name.mlopatkin")
         option("NullAway:AssertsEnabled", "true")
         option("NullAway:ExcludedClassAnnotations", "javax.annotation.Generated,javax.annotation.processing.Generated")
-        option("NullAway:ExcludedFieldAnnotations",
-                listOf("org.checkerframework.checker.nullness.qual.MonotonicNonNull",
-                        "org.mockito.Mock,org.mockito.Captor,org.mockito.Spy",
-                        "org.junit.jupiter.api.io.TempDir").joinToString(separator = ","))
+        option(
+            "NullAway:ExcludedFieldAnnotations",
+            listOf(
+                "org.checkerframework.checker.nullness.qual.MonotonicNonNull",
+                "org.mockito.Mock",
+                "org.mockito.Captor,org.mockito.Spy",
+                "org.junit.jupiter.api.io.TempDir"
+            ).joinToString(separator = ",")
+        )
         errorproneArgs.add("-Xep:NullAway:ERROR")
     }
 }
