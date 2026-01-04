54a1c5a40bdf769740122daaff007ebc6b7a3ac1
Mikhail Lopatkin <me@mlopatkin.name>
2026-01-04 19:47:58 +0100

    Add properties-based FlatLaf configuration
    This is more declarative that our code-based solution.
    
    Issue: #204

--- src/name/mlopatkin/andlogview/ui/themes/FlatLafTheme.java
+++ src/name/mlopatkin/andlogview/ui/themes/FlatLafTheme.java
@@ -16,10 +16,8 @@
 
 package name.mlopatkin.andlogview.ui.themes;
 
-import name.mlopatkin.andlogview.widgets.ConfigurableFlatBorder;
-
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.IntelliJTheme;
-import com.formdev.flatlaf.util.ColorFunctions;
 import com.google.common.io.Resources;
 
 import org.jspecify.annotations.Nullable;
@@ -27,15 +25,13 @@ import org.jspecify.annotations.Nullable;
 import java.io.IOException;
 import java.util.Locale;
 
-import javax.swing.UIManager;
-
 /**
  * FlatLaf L&amp;F with Light Flat IDEA theme.
  */
 class FlatLafTheme implements Theme {
-
     @Override
     public void install() {
+        FlatLaf.registerCustomDefaultsSource(getClass().getPackageName());
         try {
             if (!IntelliJTheme.setup(Resources.asByteSource(FlatLafThemes.LIGHTFLAT.getUrl()).openStream())) {
                 throw failure(null);
@@ -43,25 +39,6 @@ class FlatLafTheme implements Theme {
         } catch (IOException e) {
             throw failure(e);
         }
-
-        // This theme shows a thick blue outline when the JTable is focused. This doesn't look good in this app, so
-        // we are disabling it by providing a custom border for JScrollPanes wrapping JTables which are displaying
-        // the border.
-        ConfigurableFlatBorder tableBorder = new ConfigurableFlatBorder();
-        tableBorder.focusWidthProp.setDefault(0);
-        tableBorder.innerFocusWidthProp.setDefault(0f);
-        tableBorder.focusedBorderColorProp.setDefault(UIManager.getColor("Component.borderColor"));
-        UIManager.put("Table.scrollPaneBorder", tableBorder);
-        // Disable the focus border for other scroll panes too.
-        UIManager.put("ScrollPane.border", tableBorder);
-
-        var selectionBackground = UIManager.getColor("Tree.selectionBackground");
-        var dropBackground = ColorFunctions.lighten(selectionBackground, 0.1f);
-        UIManager.put("Tree.dropCellBackground", dropBackground);
-        UIManager.put("Tree.dropLineColor", dropBackground);
-
-        // Display mnemonics always, otherwise it is impossible to see them in the context menu.
-        UIManager.put("Component.hideMnemonics", false);
     }
 
     private ThemeException failure(@Nullable Throwable exception) {
